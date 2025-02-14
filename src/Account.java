835604a8e6f9a61c0b5a7d1bb8207e6502db4da8
nabini01 <nabini01@wsc.edu>
2025-02-13 21:01:42 -0600

    Documentation and @Override added

--- src/Account.java
+++ src/Account.java
@@ -4,18 +4,31 @@ public class Account {
      * Class variable for maintaining the last account number used.
      */
     private static int lastId = 0;
+
     /**
      * The unique account number assigned by the default constructor.
      */
-    private final int id;
+    private int id;
 
+    /**
+     * The account owner's first name.
+     */
     private String firstName;
 
+    /**
+     * The account owner's last name.
+     */
     private String lastName;
 
+    /**
+     * The current account balance.
+     */
     private double balance;
 
-
+    /**
+     * The default constructor sets a unique account id based on the last account id and
+     * also sets all the initial values for instance level fields.
+     */
     public Account() {
         id = ++lastId;
     }
@@ -25,11 +38,21 @@ public class Account {
 
     }
 
+    /**
+     * The overload constructor calls the default constructor and the allows overriding
+     * the default values for the account owner's name by calling the setters for data validation.
+     * @param firstName The account owner's first name.
+     * @param lastName The account owner's last name.
+     */
     public Account(String firstName, String lastName) {
 
 
     }
 
+    /**
+     * Allow retrieval of the current account balance.
+     * @return The current account balance.
+     */
     public double getBalance() {
         return balance;
     }
@@ -58,6 +81,10 @@ public class Account {
             this.lastName = lastName;
     }
 
+    /**
+     * Allows making a deposit for amounts greater than zero.
+     * @param amount The amount to add to the account balance.
+     */
     public void deposit(double amount) {
         if (amount <= 0)
             throw new IllegalArgumentException ("Invalid amount [-100.00]. Must be greater than 0.");
@@ -65,12 +92,26 @@ public class Account {
             balance += amount;
     }
 
+    /**
+     * Allows making a withdrawal for amounts greater than zero and NOT greater than the account balance.
+     * @param amount The amount to reduce the account balance.
+     */
     public void withdraw(double amount) {
-        if (amount <= 0 || amount > balance)
-            throw new IllegalArgumentException("Invalid amount [-100.00]. Must be greater than 0.");
-        throw new IllegalArgumentException("Invalid amount [-500.00]. Cannot be greater than the account balance [100.00].");
-
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Invalid amount. Must be greater than 0.");
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Invalid amount. Cannot be greater than the account balance of " + balance);
+        }
         balance -= amount;
     }
 
+    /**
+     * General account information (id and name) as a string. Example: ID #1: Nassir Binion.
+     * @return The account's id and name.
+     */
+    @Override
+    public String toString() {
+        return "Account [id#" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
 }
